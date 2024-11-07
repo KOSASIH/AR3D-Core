989cf4293f2756262f34f83fc197269e59ebcea7
KOSASIH <kosasihg88@gmail.com>
2024-11-07 08:03:45 +0700

    Create User.js

---
+++ backend/models/User.js
@@ -0,0 +1,26 @@
+// backend/models/User.js
+
+const mongoose = require('mongoose');
+
+// Define the User schema
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+// Create the User model
+const User = mongoose.model('User ', userSchema);
+
+module.exports = User;
